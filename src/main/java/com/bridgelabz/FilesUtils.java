3f7bad93c53f6bf8b6b967aa29319502de67097c
ashishmashal <ashishmashal5@gmail.com>
2022-05-25 19:26:28 +0530

    Added:Stored Payroll service into file.

---
+++ src/main/java/com/bridgelabz/FilesUtils.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+import java.io.File;
+
+public class FilesUtils {
+    public static boolean deleteFiles(File contentToDelete)
+    {
+        File[]  allContents =contentToDelete.listFiles();
+        if (allContents != null)
+        {
+            for (File file: allContents)
+            {
+                deleteFiles(file);
+            }
+        }
+        return contentToDelete.delete();
+    }
+}
